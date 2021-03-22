7c8c706fee5594e0ade32258b450d1ac4f6dde68
Craigory Coppola <craigory.coppola@universalplant.com>
2021-03-22 13:59:25 -0500

    chore(): CI publish
    Signed-off-by: Craigory Coppola <craigory.coppola@universalplant.com>

---
+++ tools/utils/get-affected.ts
@@ -0,0 +1,20 @@
+import { execSync } from "child_process";
+
+export function getAffectedProjects() {
+    const all = process.env.ALL === 'true';
+    const specific = process.env.SPECIFIC_PROJECT;
+  
+    let projects = [];
+    if (specific && specific !== 'null') {
+      projects = [specific];
+    } else {
+      const cmd = `npx nx print-affected ${all ? '--all' : '--base="HEAD~1"'} --target="build" --select=tasks.target.project`;
+  
+      console.log(cmd);
+  
+      const projectsStr = execSync(cmd).toString().trim();
+      projects = projectsStr != '' ? projectsStr.split(', ') : [];
+    }
+    return projects;
+}
+  
