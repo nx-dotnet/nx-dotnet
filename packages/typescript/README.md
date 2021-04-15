9d66e34518240d4de4431b73c43a854452071f9d
Craigory Coppola <craigory.coppola@universalplant.com>
2021-04-15 11:21:45 -0500

    feat(typescript): scaffold ts project

---
+++ packages/typescript/README.md
@@ -0,0 +1,7 @@
+# typescript
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test typescript` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/typescript/src/generators/typescript/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { TypescriptGeneratorSchema } from './schema';
+
+describe('typescript generator', () => {
+  let appTree: Tree;
+  const options: TypescriptGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
