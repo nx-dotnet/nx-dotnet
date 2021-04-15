501bf643aee3033eb15daf911def0c35d9f774b7
Craigory Coppola <craigory.coppola@universalplant.com>
2021-04-15 11:21:45 -0500

    feat(typescript): scaffold ts project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/packages/core', '<rootDir>/e2e\\core-e2e'],
+  projects: [
+    '<rootDir>/packages/core',
+    '<rootDir>/e2e\\core-e2e',
+    '<rootDir>/packages/typescript',
+    '<rootDir>/e2e\\typescript-e2e',
+  ],
 };
