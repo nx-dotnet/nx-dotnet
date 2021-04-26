d07b9f306677110c491da2c9a2eb075d79835c1a
Craigory Coppola <craigory.coppola@universalplant.com>
2021-04-26 15:19:54 -0500

    fix(core): packages not published by last CI run

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,41 @@
 module.exports = {
   branches: ['master', { name: 'dev', prerelease: true }],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', release: false },
+          { type: 'test', release: false },
+          { scope: 'ci', release: false },
+          { scope: 'repo', release: false },
+          { type: 'release', release: false },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        presetConfig: {
+          types: [
+            { type: 'feat', hidden: false },
+            { type: 'fix', hidden: false },
+            { type: 'chore', hidden: true },
+            { type: 'docs', hidden: true },
+            { type: 'style', hidden: true },
+            { type: 'refactor', hidden: true },
+            { type: 'test', hidden: true },
+            { type: 'release', hidden: true },
+          ],
+        },
+      },
+    ],
     '@semantic-release/changelog',
     [
       '@semantic-release/exec',
       {
-        publish: [
+        publishCmd: [
           'yarn ts-node tools/scripts/publish-all ${nextRelease.version} ${nextRelease.channel}',
         ],
       },
