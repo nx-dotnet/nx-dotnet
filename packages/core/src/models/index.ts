d320ce8579d62b2953d8646ae2bf4eec11018820
Ben Callaghan <bcallaghan@selectbankcard.com>
2021-05-19 13:24:31 -0600

    feat(core): add lint config to generated projects
    Add lint configuration to all new projects for user convenience.

--- packages/core/src/models/index.ts
+++ packages/core/src/models/index.ts
@@ -2,3 +2,4 @@ export * from './build-executor-configuration';
 export * from './project-generator-schema';
 export * from './serve-executor-configuration';
 export * from './test-executor-configuration';
+export * from './lint-executor-configuration';
