e391744c3cd261c6dc462a68335dfd602b928b58
Ben Callaghan <bcallaghan@selectbankcard.com>
2021-05-19 13:42:25 -0600

    feat(core): add migration to add lint target
    Add a migration that updates existing projects with the new lint target.

---
+++ packages/core/src/migrations/add-lint-target/add-lint-target.ts
@@ -0,0 +1,11 @@
+import { Tree, updateProjectConfiguration } from '@nrwl/devkit';
+import { getNxDotnetProjects } from '@nx-dotnet/utils';
+import { GetLintExecutorConfiguration } from '../../models';
+
+export default function update(host: Tree) {
+  const projects = getNxDotnetProjects(host);
+  for (const [name, project] of projects) {
+    project.targets.lint ??= GetLintExecutorConfiguration();
+    updateProjectConfiguration(host, name, project);
+  }
+}
