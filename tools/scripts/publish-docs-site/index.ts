e259a28c5df552a3c85fa7b154c47fe2fc19ed61
Craigory Coppola <craigorycoppola@gmail.com>
2021-05-23 00:52:02 -0500

    fix(docs-site): deploy on commit to master

--- tools/scripts/publish-docs-site/index.ts
+++ tools/scripts/publish-docs-site/index.ts
@@ -3,5 +3,9 @@ import { execSync } from "child_process";
 
 if (require.main === module) {
     const path = joinPathFragments(__dirname, '../../../dist/apps/docs-site');
-    execSync('git init && git checkout -b "gh-pages" && git add . && git commit -m "chore: deploy docs site" && git remote add origin https://github.com/nx-dotnet/nx-dotnet && git push -f --set-upstream origin gh-pages', {cwd: path, stdio: 'inherit'})
+    execSync('git init ', {cwd: path, stdio: 'inherit'})
+    execSync('git checkout -b "gh-pages"', {cwd: path, stdio: 'inherit'})
+    execSync('git add . && git commit -m "chore: deploy docs site"', {cwd: path, stdio: 'inherit'})
+    execSync(`git remote add origin https://github-actions:${process.env.GITHUB_TOKEN}@github.com/nx-dotnet/nx-dotnet`, {cwd: path, stdio: 'inherit'})
+    execSync('git push -f --set-upstream origin gh-pages', {cwd: path, stdio: 'inherit'})
 }
