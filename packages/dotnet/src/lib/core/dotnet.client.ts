cb9a4c7e429343e528df8b8f29a7b90975b5d830
Paulo Oliveira <asinino@gmail.com>
2022-03-02 18:21:50 +0000

    fix(dotnet): parameters are managed by array of strings

--- packages/dotnet/src/lib/core/dotnet.client.ts
+++ packages/dotnet/src/lib/core/dotnet.client.ts
@@ -1,6 +1,5 @@
+import { getParameterArrayStrings, swapKeysUsingMap } from '@nx-dotnet/utils';
 import { ChildProcess, execSync, spawn } from 'child_process';
-
-import { getParameterString, swapKeysUsingMap } from '@nx-dotnet/utils';
 
 import {
   addPackageKeyMap,
@@ -24,24 +23,22 @@ import { LoadedCLI } from './dotnet.factory';
 export class DotNetClient {
   constructor(private cliCommand: LoadedCLI, public cwd?: string) {}
 
-  new(template: dotnetTemplate, parameters?: dotnetNewOptions): void {
-    let cmd = `${this.cliCommand.command} new ${template}`;
+  new(template: dotnetTemplate, parameters?: dotnetNewOptions): Buffer {
+    const params = [`new`, template];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, newKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
-    return this.logAndExecute(cmd);
+    return this.logAndExecute(params);
   }
 
-  build(project: string, parameters?: dotnetBuildOptions): void {
-    let cmd = `${this.cliCommand.command} build "${project}"`;
+  build(project: string, parameters?: dotnetBuildOptions): Buffer {
+    const params = [`build`, project];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, buildKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
-    return this.logAndExecute(cmd);
+    return this.logAndExecute(params);
   }
 
   run(
@@ -49,46 +46,35 @@ export class DotNetClient {
     watch = false,
     parameters?: dotnetRunOptions,
   ): ChildProcess {
-    let cmd = watch
-      ? `watch --project "${project}" run`
-      : `run --project "${project}"`;
+    const params = watch
+      ? [`watch`, `--project`, project, `run`]
+      : [`run`, `--project`, project];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, runKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
-    console.log(`Executing Command: dotnet ${cmd}`);
-    return spawn(this.cliCommand.command, cmd.split(' '), {
-      stdio: 'inherit',
-      cwd: this.cwd,
-    });
+
+    return this.logAndSpawn(params);
   }
 
   test(
     project: string,
     watch?: boolean,
     parameters?: dotnetTestOptions,
-  ): void | ChildProcess {
-    let cmd = watch ? `watch --project "${project}" test` : `test "${project}"`;
-    cmd = `${this.cliCommand.command} ${cmd}`;
+  ): Buffer | ChildProcess {
+    const params = watch
+      ? [`watch`, `--project`, project, `test`]
+      : [`test`, project];
 
     if (parameters) {
-      const mappedParameters = swapKeysUsingMap(parameters, testKeyMap);
-      const paramString = getParameterString(mappedParameters);
-      cmd = `${cmd} ${paramString}`;
+      parameters = swapKeysUsingMap(parameters, testKeyMap);
+      params.push(...getParameterArrayStrings(parameters));
     }
     if (!watch) {
-      return this.logAndExecute(cmd);
+      return this.logAndExecute(params);
     } else {
-      console.log(`Executing Command: ${cmd}`);
-      const params = cmd
-        .split(' ')
-        .slice(1)
-        .filter((x) => x.length);
-      return spawn(this.cliCommand.command, params, {
-        stdio: 'inherit',
-        cwd: this.cwd,
-      });
+      const slicedParams = params.slice(1).filter((x) => x.length);
+      return this.logAndSpawn(slicedParams);
     }
   }
 
@@ -96,20 +82,17 @@ export class DotNetClient {
     project: string,
     pkg: string,
     parameters?: dotnetAddPackageOptions,
-  ): void {
-    let cmd = `${this.cliCommand.command} add "${project}" package ${pkg}`;
+  ): Buffer {
+    const params = [`add`, project, `package`, pkg];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, addPackageKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
-    return this.logAndExecute(cmd);
+    return this.logAndExecute(params);
   }
 
-  addProjectReference(hostCsProj: string, targetCsProj: string): void {
-    return this.logAndExecute(
-      `${this.cliCommand.command} add ${hostCsProj} reference ${targetCsProj}`,
-    );
+  addProjectReference(hostCsProj: string, targetCsProj: string): Buffer {
+    return this.logAndExecute([`add`, hostCsProj, `reference`, targetCsProj]);
   }
 
   publish(
@@ -117,67 +100,78 @@ export class DotNetClient {
     parameters?: dotnetPublishOptions,
     publishProfile?: string,
     extraParameters?: string,
-  ): void {
-    let cmd = `${this.cliCommand.command} publish "${project}"`;
+  ): Buffer {
+    const params = [`publish`, `"${project}"`];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, publishKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
     if (publishProfile) {
-      cmd = `${cmd} -p:PublishProfile=${publishProfile}`;
+      params.push(`-p:PublishProfile=${publishProfile}`);
     }
     if (extraParameters) {
-      cmd = `${cmd} ${extraParameters}`;
+      params.push(`${extraParameters}`);
     }
+    return this.logAndExecute(params);
+  }
+
+  installTool(tool: string): Buffer {
+    const cmd = [`tool`, `install`, tool];
     return this.logAndExecute(cmd);
   }
 
-  installTool(tool: string): void {
-    const cmd = `${this.cliCommand.command} tool install ${tool}`;
+  restorePackages(project: string): Buffer {
+    const cmd = [`restore`, project];
     return this.logAndExecute(cmd);
   }
 
-  restorePackages(project: string): void {
-    const cmd = `${this.cliCommand.command} restore "${project}"`;
+  restoreTools(): Buffer {
+    const cmd = [`tool`, `restore`];
     return this.logAndExecute(cmd);
   }
 
-  restoreTools(): void {
-    const cmd = `${this.cliCommand.command} tool restore`;
-    return this.logAndExecute(cmd);
-  }
-
-  format(project: string, parameters?: dotnetFormatOptions): void {
-    let cmd = `${this.cliCommand.command} format "${project}"`;
+  format(project: string, parameters?: dotnetFormatOptions): Buffer {
+    const params = [`format`, project];
     if (parameters) {
       parameters = swapKeysUsingMap(parameters, formatKeyMap);
-      const paramString = parameters ? getParameterString(parameters) : '';
-      cmd = `${cmd} ${paramString}`;
+      params.push(...getParameterArrayStrings(parameters));
     }
-    return this.logAndExecute(cmd);
+    return this.logAndExecute(params);
   }
 
   addProjectToSolution(solutionFile: string, project: string) {
-    const cmd = `${this.cliCommand.command} sln "${solutionFile}" add "${project}"`;
-    this.logAndExecute(cmd);
+    const params = [`sln`, solutionFile, `add`, project];
+    this.logAndExecute(params);
   }
 
   getSdkVersion(): Buffer {
-    const cmd = 'dotnet --version';
-    return this.execute(cmd);
+    return this.execute(['--version']);
   }
 
   printSdkVersion(): void {
-    this.logAndExecute('dotnet --version');
+    this.logAndExecute(['--version']);
   }
 
-  private logAndExecute(cmd: string): void {
-    console.log(`Executing Command: ${cmd}`);
-    execSync(cmd, { stdio: 'inherit', cwd: this.cwd || process.cwd() });
+  private logAndExecute(params: string[]): Buffer {
+    console.log(
+      `Executing Command: ${this.cliCommand.command} ${params.join(', ')}`,
+    );
+    return this.execute(params);
   }
 
-  private execute(cmd: string): Buffer {
-    return execSync(cmd, { cwd: this.cwd || process.cwd() });
+  private execute(params: string[]): Buffer {
+    return execSync([this.cliCommand.command, ...params].join(), {
+      cwd: this.cwd || process.cwd(),
+    });
+  }
+
+  private logAndSpawn(params: string[]): ChildProcess {
+    console.log(
+      `Executing Command: ${this.cliCommand.command} ${params.join(', ')}`,
+    );
+    return spawn(this.cliCommand.command, params, {
+      stdio: 'inherit',
+      cwd: this.cwd || process.cwd(),
+    });
   }
 }
