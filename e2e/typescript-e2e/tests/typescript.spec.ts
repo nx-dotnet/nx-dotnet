bea5813e3c9aef01fcecc592b7dbc6dd61195177
Craigory Coppola <craigorycoppola@gmail.com>
2022-05-11 15:57:50 -0400

    chore(repo): remove ts package

--- e2e/typescript-e2e/tests/typescript.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('typescript e2e', () => {
-  it('has a test', () => {
-    expect(true).toBeTruthy();
-  });
-});
--- packages/typescript/src/generators/typescript/schema.d.ts
+++
@@ -1,5 +0,0 @@
-export interface TypescriptGeneratorSchema {
-  name: string;
-  tags?: string;
-  directory?: string;
-}
