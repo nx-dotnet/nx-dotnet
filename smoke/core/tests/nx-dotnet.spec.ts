1edf360a0e2e90623181dd58842147c2b7cff7f5
AgentEnder <craigorycoppola@gmail.com>
2022-05-23 10:49:09 -0400

    chore(repo): changes to smoke

--- smoke/core/tests/nx-dotnet.spec.ts
+++ smoke/core/tests/nx-dotnet.spec.ts
@@ -1,14 +1,15 @@
 import { uniq } from '@nrwl/nx-plugin/testing';
 
 import { execSync, ExecSyncOptions } from 'child_process';
-import { ensureDirSync } from 'fs-extra';
 import { join } from 'path';
 
-import { rimraf } from '@nx-dotnet/utils';
 import { readDependenciesFromNxDepGraph } from '@nx-dotnet/utils/e2e';
+import { dirSync } from 'tmp';
 
-const smokeDirectory = 'tmp/smoke-core';
-const execSyncOptions: ExecSyncOptions = {
+let smokeDirectory: string;
+let cleanup: () => void;
+
+const execSyncOptions: () => ExecSyncOptions = () => ({
   cwd: join(smokeDirectory, 'test'),
   env: {
     ...process.env,
@@ -18,15 +19,20 @@ const execSyncOptions: ExecSyncOptions = {
     GIT_AUTHOR_EMAIL: 'no-reply@fake.com',
   },
   stdio: 'inherit',
-};
+});
 
 const testApp = uniq('test-app');
 const testLib = uniq('test-lib');
 
 describe('nx-dotnet smoke', () => {
   beforeEach(async () => {
-    await rimraf(smokeDirectory);
-    ensureDirSync(smokeDirectory);
+    ({ name: smokeDirectory, removeCallback: cleanup } = dirSync({
+      unsafeCleanup: true,
+    }));
+  });
+
+  afterEach(async () => {
+    cleanup();
   });
 
   it('should work', async () => {
@@ -38,27 +44,24 @@ describe('nx-dotnet smoke', () => {
         stdio: 'inherit',
       },
     );
-    execSync(
-      'git init --initial-branch=main && git add . && git commit -m "initial commit"',
-      execSyncOptions,
-    );
-    execSync('npm i --save-dev @nx-dotnet/core', execSyncOptions);
+
+    execSync('npm i --save-dev @nx-dotnet/core', execSyncOptions());
     execSync(
       `npx nx g @nx-dotnet/core:lib ${testLib} --language C# --template classlib --testTemplate nunit`,
-      execSyncOptions,
+      execSyncOptions(),
     );
     execSync(
       `npx nx g @nx-dotnet/core:app ${testApp} --language C# --template webapi --testTemplate nunit`,
-      execSyncOptions,
+      execSyncOptions(),
     );
 
     execSync(
       `npx nx g @nx-dotnet/core:project-reference ${testApp} ${testLib}`,
-      execSyncOptions,
+      execSyncOptions(),
     );
 
     execSync('npx nx print-affected --target build', {
-      ...execSyncOptions,
+      ...execSyncOptions(),
       stdio: ['ignore', 'ignore', 'inherit'],
     });
 
@@ -68,7 +71,7 @@ describe('nx-dotnet smoke', () => {
     );
     expect(deps).toContain(testLib);
 
-    execSync(`npx nx build ${testApp}`, execSyncOptions);
+    execSync(`npx nx build ${testApp}`, execSyncOptions());
 
     expect(true).toBeTruthy();
   }, 1500000);
