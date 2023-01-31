483086b40308156984bd463bdb28799a260292cd
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-01-31 22:59:32 +0100

    feat(core): add support for notDependOnLibsWithTags module boundaries (#592)
    fixes https://github.com/nx-dotnet/nx-dotnet/issues/564

--- packages/core/src/tasks/check-module-boundaries.spec.ts
+++ packages/core/src/tasks/check-module-boundaries.spec.ts
@@ -1,7 +1,8 @@
 import { Tree, writeJson } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-
 import * as ESLintNamespace from 'eslint';
+import * as fastGlob from 'fast-glob';
+import { vol } from 'memfs';
 
 import {
   CONFIG_FILE_PATH,
@@ -21,7 +22,7 @@ const MOCK_BOUNDARIES: ModuleBoundaries = [
     sourceTag: 'a',
   },
   {
-    onlyDependOnLibsWithTags: ['b', 'shared'],
+    notDependOnLibsWithTags: ['a', 'shared'],
     sourceTag: 'b',
   },
   {
@@ -74,7 +75,32 @@ describe('load-module-boundaries', () => {
   });
 });
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+jest.mock('fs', () => require('memfs').fs);
+
 describe('enforce-module-boundaries', () => {
+  beforeEach(() => {
+    const appTree = createTreeWithEmptyWorkspace();
+    jest.spyOn(ESLintNamespace, 'ESLint').mockReturnValue({
+      calculateConfigForFile: jest.fn().mockResolvedValue({
+        rules: {
+          '@nrwl/nx/enforce-module-boundaries': [
+            1,
+            { depConstraints: MOCK_BOUNDARIES },
+          ],
+        },
+      }),
+    } as unknown as ESLintNamespace.ESLint);
+    writeJson<NxDotnetConfig>(appTree, CONFIG_FILE_PATH, {
+      nugetPackages: {},
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    vol.reset();
+  });
+
   it('should exit early if no tags on project', async () => {
     const spy = jest.spyOn(checkModule, 'loadModuleBoundaries');
     const results = await checkModuleBoundariesForProject('a', {
@@ -85,6 +111,78 @@ describe('enforce-module-boundaries', () => {
       },
     });
     expect(spy).not.toHaveBeenCalled();
+    expect(results).toHaveLength(0);
+  });
+
+  it('should find violations with onlyDependOnLibsWithTags', async () => {
+    const globResults = ['libs/a/a.csproj'];
+    jest.spyOn(fastGlob, 'sync').mockImplementation(() => globResults);
+
+    vol.fromJSON({
+      'libs/a/a.csproj':
+        '<Project Sdk="Microsoft.NET.Sdk.Web"><ItemGroup><ProjectReference Include="..\\..\\libs\\ui\\ui.csproj" /></ItemGroup></Project>',
+    });
+
+    const results = await checkModuleBoundariesForProject('a', {
+      a: {
+        tags: ['a'],
+        targets: { ui: {} },
+        root: 'libs/a',
+      },
+      ui: {
+        tags: ['ui'],
+        targets: {},
+        root: 'libs/ui',
+      },
+    });
+    expect(results).toHaveLength(1);
+  });
+
+  it('should find violations with notDependOnLibsWithTags', async () => {
+    const globResults = ['libs/b/b.csproj'];
+    jest.spyOn(fastGlob, 'sync').mockImplementation(() => globResults);
+
+    vol.fromJSON({
+      'libs/b/b.csproj':
+        '<Project Sdk="Microsoft.NET.Sdk.Web"><ItemGroup><ProjectReference Include="..\\..\\libs\\a\\a.csproj" /></ItemGroup></Project>',
+    });
+
+    const results = await checkModuleBoundariesForProject('b', {
+      a: {
+        tags: ['a'],
+        targets: {},
+        root: 'libs/a',
+      },
+      b: {
+        tags: ['b'],
+        targets: { a: {} },
+        root: 'libs/b',
+      },
+    });
+    expect(results).toHaveLength(1);
+  });
+
+  it('should pass without violations', async () => {
+    const globResults = ['libs/a/a.csproj'];
+    jest.spyOn(fastGlob, 'sync').mockImplementation(() => globResults);
+
+    vol.fromJSON({
+      'libs/a/a.csproj':
+        '<Project Sdk="Microsoft.NET.Sdk.Web"><ItemGroup><ProjectReference Include="..\\..\\libs\\shared\\shared.csproj" /></ItemGroup></Project>',
+    });
+
+    const results = await checkModuleBoundariesForProject('a', {
+      a: {
+        tags: ['a'],
+        targets: { shared: {} },
+        root: 'libs/a',
+      },
+      shared: {
+        tags: ['shared'],
+        targets: {},
+        root: 'libs/shared',
+      },
+    });
     expect(results).toHaveLength(0);
   });
 });
