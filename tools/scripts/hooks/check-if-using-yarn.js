fbf0af6931274fec80a43ace5bff4b095488c959
AgentEnder <craigorycoppola@gmail.com>
2023-02-22 14:38:09 -0500

    chore(repo): migrate-next

--- tools/scripts/hooks/check-if-using-yarn.js
+++ tools/scripts/hooks/check-if-using-yarn.js
@@ -3,6 +3,7 @@ const { basename } = require('path');
 exports.checkIfYarn = () => {
   return (
     process.env.FORCE_YARN_CHECK === 'true' ||
+    process.env.NX_CLI_SET === 'true' ||
     basename(process.env.npm_execpath || '').startsWith('yarn')
   );
 };
--- tools/scripts/publish-dev/index.ts
+++ tools/scripts/publish-dev/index.ts
@@ -1,19 +1,31 @@
 import { readJson } from '../../utils';
-import { PublishAll } from '../publish-all';
+import { publishAll } from '../publish-all';
 
-export function main(all = false, specific?: string) {
+import * as parser from 'yargs-parser';
+
+export function main(version: string) {
   const rootPkg = readJson('package.json');
-
-  const [prev, tagSpec] = rootPkg.version.split('-');
-  let [tag, rev] = tagSpec ? tagSpec.split('.') : ['dev', '0'];
+  const [next, tagSpec]: [string, string | null] = rootPkg.version.startsWith(
+    version,
+  )
+    ? rootPkg.version.split('-')
+    : [version, null];
+  let [tag, rev] = tagSpec ? tagSpec.split('.') : ['beta', '0'];
   rev = (parseInt(rev) + 1).toString();
   rev = rev === 'NaN' ? '0' : rev;
-  const newVersion = `${prev}-${tag}.${rev}`;
-  console.log('New Version: ', { newVersion, prev, tag, rev });
+  const newVersion = `${next}-${tag}.${rev}`;
 
-  PublishAll(newVersion, tag);
+  publishAll(newVersion, tag);
 }
 
 if (require.main === module) {
-  main();
+  const { version } = parser(process.argv, {
+    string: ['version'],
+  }) as parser.Arguments & {
+    version: string;
+  };
+  if (!version) {
+    throw new Error('Version is required');
+  }
+  main(version);
 }
