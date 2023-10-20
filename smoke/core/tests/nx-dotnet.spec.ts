6438e04568c083cc1b821b013a48523ffccebb5c
Craigory Coppola <craigorycoppola@gmail.com>
2023-10-19 23:06:03 -0400

    chore(repo): update smoke tests

--- smoke/core/tests/nx-dotnet.spec.ts
+++ smoke/core/tests/nx-dotnet.spec.ts
@@ -1,15 +1,16 @@
 import { uniq } from '@nx/plugin/testing';
 
-import { execSync, ExecSyncOptions } from 'child_process';
+import { exec, ExecOptions } from 'child_process';
 import { join } from 'path';
 
 import { readDependenciesFromNxDepGraph } from '@nx-dotnet/utils/e2e';
 import { dirSync } from 'tmp';
+import { existsSync, mkdirSync } from 'fs';
 
 let smokeDirectory: string;
 let cleanup: () => void;
 
-const execSyncOptions: () => ExecSyncOptions = () => ({
+const execAsyncOptions: () => ExecOptions = () => ({
   cwd: join(smokeDirectory, 'test'),
   env: {
     ...process.env,
@@ -17,56 +18,71 @@ const execSyncOptions: () => ExecSyncOptions = () => ({
     GIT_COMMITTER_EMAIL: 'no-reply@fake.com',
     GIT_AUTHOR_NAME: 'Smoke Test CI',
     GIT_AUTHOR_EMAIL: 'no-reply@fake.com',
+    ISTTY: 'false',
   },
-  stdio: 'inherit',
 });
 
 const testApp = uniq('test-app');
 const testLib = uniq('test-lib');
 
+function execAsync(command: string, opts: ExecOptions): Promise<void> {
+  opts = { ...execAsyncOptions(), ...opts };
+  return new Promise((resolve, reject) => {
+    const cp = exec(command, opts, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      }
+      resolve();
+    });
+
+    cp.stdout?.pipe(process.stdout);
+    cp.stderr?.pipe(process.stderr);
+  });
+}
+
 describe('nx-dotnet smoke', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     ({ name: smokeDirectory, removeCallback: cleanup } = dirSync({
       unsafeCleanup: true,
     }));
-  });
 
-  afterEach(async () => {
-    cleanup();
-  });
-
-  it('should work', async () => {
-    execSync(
+    await execAsync(
       'npx create-nx-workspace@latest test --preset ts --nxCloud false',
       {
         cwd: smokeDirectory,
         env: process.env,
-        stdio: 'inherit',
       },
     );
 
-    execSync('git init', execSyncOptions());
+    await execAsync('git init', await execAsyncOptions());
 
-    execSync('npm i --save-dev @nx-dotnet/core', execSyncOptions());
-    execSync(
+    await execAsync(
+      'npm i --save-dev @nx-dotnet/core',
+      await execAsyncOptions(),
+    );
+  });
+
+  afterAll(async () => {
+    cleanup();
+  });
+
+  it('should be able to generate apps, libs, and references', async () => {
+    await execAsync(
       `npx nx g @nx-dotnet/core:lib ${testLib} --language C# --template classlib --testTemplate nunit`,
-      execSyncOptions(),
+      await execAsyncOptions(),
     );
-    execSync(
+    await execAsync(
       `npx nx g @nx-dotnet/core:app ${testApp} --language C# --template webapi --testTemplate nunit`,
-      execSyncOptions(),
+      await execAsyncOptions(),
     );
 
-    execSync(
+    await execAsync(
       `npx nx g @nx-dotnet/core:project-reference ${testApp} ${testLib}`,
-      execSyncOptions(),
+      await execAsyncOptions(),
     );
 
-    execSync(`git commit -am "chore: scaffold projects"`, execSyncOptions());
-
-    execSync('npx nx print-affected --target build', {
-      ...execSyncOptions(),
-      stdio: ['ignore', 'ignore', 'inherit'],
+    await execAsync(`npx nx build ${testApp} --graph=stdout`, {
+      ...(await execAsyncOptions()),
     });
 
     const deps = await readDependenciesFromNxDepGraph(
@@ -75,8 +91,19 @@ describe('nx-dotnet smoke', () => {
     );
     expect(deps).toContain(testLib);
 
-    execSync(`npx nx build ${testApp}`, execSyncOptions());
+    await execAsync(`npx nx build ${testApp}`, await execAsyncOptions());
+  }, 1500000);
 
-    expect(true).toBeTruthy();
+  it('should be able infer projects', async () => {
+    const projectDirectory = join(smokeDirectory, 'test', 'apps', 'inferred');
+    ensureDirSync(projectDirectory);
+    await execAsync('dotnet new webapi', { cwd: projectDirectory });
+    await execAsync(`npx nx build`, { cwd: projectDirectory });
   }, 1500000);
 });
+
+function ensureDirSync(dir: string) {
+  if (!existsSync(dir)) {
+    mkdirSync(dir, { recursive: true });
+  }
+}
