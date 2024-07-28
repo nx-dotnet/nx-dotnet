562c7fa26973e86618b2b285f1cd8466d4220f8a
Craigory Coppola <craigorycoppola@gmail.com>
2024-07-28 17:30:45 -0500

    chore(repo): move to nx release (#874)

--- tools/scripts/start-local-registry.ts
+++ tools/scripts/start-local-registry.ts
@@ -3,25 +3,33 @@
  * It is meant to be called in jest's globalSetup.
  */
 import { startLocalRegistry } from '@nx/js/plugins/jest/local-registry';
-import { publishAll } from './publish-all';
-import { copySync } from 'fs-extra';
+import { releasePublish, releaseVersion } from 'nx/release';
 
 export default async () => {
   // local registry target to run
-  const localRegistryTarget = 'local-registry';
+  const localRegistryTarget = '@nx-dotnet/source:local-registry';
   // storage folder for the local registry
   const storage = './tmp/local-registry/storage';
-
-  console.log('STARTING LOCAL REGISTRY');
 
   (global as any).stopLocalRegistry = await startLocalRegistry({
     localRegistryTarget,
     storage,
-    verbose: false,
+    verbose: process.env.NX_VERBOSE_LOGGING === 'true',
   });
 
-  console.log('PUBLISHING PACKAGES TO LOCAL REGISTRY');
-  process.env.NX_DOTNET_E2E = 'true';
-  copySync('.npmrc.local', '.npmrc');
-  await publishAll('99.99.99', 'local');
+  await releaseVersion({
+    specifier: '0.0.0-e2e',
+    stageChanges: false,
+    gitCommit: false,
+    gitTag: false,
+    firstRelease: true,
+    generatorOptionsOverrides: {
+      skipLockFileUpdate: true,
+      currentVersionResolver: 'registry',
+    },
+  });
+  await releasePublish({
+    tag: 'e2e',
+    firstRelease: true,
+  });
 };
